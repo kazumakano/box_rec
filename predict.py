8fbd87bc299eddd71648cf197a086e87316b3038
kazumakano <kazumakano@outlook.jp>
2025-02-05 10:41:45 +0900

    minor update

--- predict.py
+++ predict.py
@@ -22,7 +22,7 @@ def _compute_offset(pjs: dict[str, np.ndarray]) -> tuple[float, float]:
 
     return tuple(offset)
 
-def predict(box_info_file: str, gpu_id: int, model_file: str, pj_file: str, result_dir: str, vid_file: str) -> None:
+def predict(box_info_file: str, gpu_id: int, model_file: str, pj_file: str, result_file: str, vid_file: str) -> None:
     offset = _compute_offset({cn: np.array(p["projective_matrix"], dtype=np.float32) for cn, p in util.load_param(pj_file).items()})
     box_info = pd.read_csv(box_info_file, usecols=("no", "x", "y"))
     box_info.loc[:, "x"] -= offset[0]
@@ -31,7 +31,7 @@ def predict(box_info_file: str, gpu_id: int, model_file: str, pj_file: str, resu
     device = torch.device("cuda", gpu_id)
     model = jit.load(model_file, map_location=device)
 
-    with open(path.join(result_dir, "predict_results.csv"), mode="w") as f:
+    with open(result_file, mode="w") as f:
         writer = csv.writer(f)
         writer.writerow(("frm_idx", "no", "cls", "conf"))
 
@@ -50,7 +50,7 @@ def predict(box_info_file: str, gpu_id: int, model_file: str, pj_file: str, resu
 
                 p: np.ndarray
                 for i, p in enumerate(softmax(output.cpu().numpy(), axis=1)):
-                    writer.writerow((int(cap.get(cv2.CAP_PROP_POS_FRAMES)) - 1, int(box_info.iloc[i]["no"]), util.look_up_key_from_val(data.USAGE, p.argmax()), p.max()))
+                    writer.writerow((int(cap.get(cv2.CAP_PROP_POS_FRAMES)) - 1, int(box_info.iloc[i]["no"]), util.look_up_key_from_val(data.USAGE, p.argmax()), format(p.max(), ".2f")))
 
                 bar.update()
 
@@ -61,9 +61,9 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--box_info_file", required=True, help="specify box info file", metavar="PATH_TO_BOX_INFO_FILE")
     parser.add_argument("-m", "--model_file", required=True, help="specify compiled model file", metavar="PATH_TO_MODEL_FILE")
     parser.add_argument("-p", "--pj_file", required=True, help="specify projection matrix file", metavar="PATH_TO_PJ_FILE")
-    parser.add_argument("-r", "--result_dir", required=True, help="specify result directory", metavar="PATH_TO_RESULT_DIR")
+    parser.add_argument("-r", "--result_file", required=True, help="specify result file", metavar="PATH_TO_RESULT_FILE")
     parser.add_argument("-v", "--vid_file", required=True, help="specify stitched video file", metavar="PATH_TO_VID_FILE")
     parser.add_argument("-g", "--gpu_id", default=0, type=int, help="specify GPU device ID", metavar="GPU_ID")
     args = parser.parse_args()
 
-    predict(args.box_info_file, args.gpu_id, args.model_file, args.pj_file, args.result_dir, args.vid_file)
+    predict(args.box_info_file, args.gpu_id, args.model_file, args.pj_file, args.result_file, args.vid_file)
